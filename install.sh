cb059b3201b9179f2da85658c45ad6660033452e
Steven Jennison <stvnjennison@gmail.com>
2016-04-19 15:38:14 -0400

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+wget https://github.com/sjennison/default-home-dir/blob/master/.bashrc
+wget https://github.com/sjennison/default-home-dir/blob/master/.git-completion.sh
+chmod +x .git-completion.sh
+mkdir .ssh
+cd .ssh
+wget https://github.com/sjennison/default-home-dir/blob/master/.ssh/authorized_keys
+source ~/.bashrc
