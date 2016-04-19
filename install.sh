c5d553e241136f15e47e30deaa2355a04a954204
Steven Jennison <stvnjennison@gmail.com>
2016-04-19 15:45:04 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #/bin/bash
-wget -O https://github.com/sjennison/default-home-dir/raw/master/.bashrc > ~/.bashrc
-wget -O https://github.com/sjennison/default-home-dir/raw/master/.git-completion.sh > ~/.git-completion.sh
+wget -O - https://github.com/sjennison/default-home-dir/raw/master/.bashrc > ~/.bashrc
+wget -O - https://github.com/sjennison/default-home-dir/raw/master/.git-completion.sh > ~/.git-completion.sh
 chmod +x .git-completion.sh
 mkdir ~/.ssh
 wget -O - https://github.com/sjennison/default-home-dir/raw/master/.ssh/authorized_keys >> ~/.ssh/authorized_keys
