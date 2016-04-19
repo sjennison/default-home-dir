ba9ed76be18b66696f80d50336430b13942756c8
Steven Jennison <stvnjennison@gmail.com>
2016-04-19 15:40:28 -0400

    Update install.sh
    Change to raw url

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #/bin/bash
-wget https://github.com/sjennison/default-home-dir/blob/master/.bashrc
-wget https://github.com/sjennison/default-home-dir/blob/master/.git-completion.sh
+wget https://github.com/sjennison/default-home-dir/raw/master/.bashrc
+wget https://github.com/sjennison/default-home-dir/raw/master/.git-completion.sh
 chmod +x .git-completion.sh
 mkdir .ssh
 cd .ssh
-wget https://github.com/sjennison/default-home-dir/blob/master/.ssh/authorized_keys
+wget https://github.com/sjennison/default-home-dir/raw/master/.ssh/authorized_keys
 source ~/.bashrc
